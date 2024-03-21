9b9771d430f36d10e589d6c6d4792009ac2a4f45
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-03-22 01:33:54 +0300

    class-functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(){
+return ([`Antonia`,`Nuru`])
+}
+ const returnLastTwoDrivers=function(){
+    return([`Amari`,`Mo`])
+ }
+
+const selectingDrivers=([returnFirstTwoDrivers,returnLastTwoDrivers])
+
+function createFareMultiplier(a){
+   return function(fare){
+     return (fare*a)
+   }
+}
+const fareDoubler=createFareMultiplier(2)
+const fareTripler=createFareMultiplier(3)
+
+const selectDifferentDrivers=function(returnLastTwoDrivers,returnFirstTwoDrivers){
+   return returnFirstTwoDrivers(returnLastTwoDrivers)
+}
+
+   
+
